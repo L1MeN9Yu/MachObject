d11ed16e16fff2512fbb7f1e98179def659939aa
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-08-26 16:51:07 +0800

    update header

--- Sources/MachOParser/MachOTypes/Mach/SubTypes/Mach.Header.swift
+++ Sources/MachOParser/MachOTypes/Mach/SubTypes/Mach.Header.swift
@@ -9,31 +9,77 @@ extension Mach {
         case _32(MachHeader32)
         case _64(MachHeader64)
 
-        var fileType: FileType {
+        var magic: UInt32 {
             switch self {
             case let ._32(header):
-                return FileType(fileType: header.fileType)
+                return header.magic
             case let ._64(header):
-                return FileType(fileType: header.fileType)
-            }
-        }
-
-        var cpuType: CPUType {
-            switch self {
-            case let ._32(header):
-                return CPUType(cpuType: header.cpuType)
-            case let ._64(header):
-                return CPUType(cpuType: header.cpuType)
-            }
-        }
-
-        var readableFlag: Set<MachHeaderFlag> {
-            switch self {
-            case let ._32(header):
-                return Set<MachHeaderFlag>(rawValue: Int(header.flags))
-            case let ._64(header):
-                return Set<MachHeaderFlag>(rawValue: Int(header.flags))
+                return header.magic
             }
         }
     }
+}
+
+public extension Mach.Header {
+    var rawCpuType: cpu_type_t {
+        switch self {
+        case let ._32(header):
+            return header.cpuType
+        case let ._64(header):
+            return header.cpuType
+        }
+    }
+
+    var rawCpuSubtype: cpu_subtype_t {
+        switch self {
+        case let ._32(header):
+            return header.cpuSubtype
+        case let ._64(header):
+            return header.cpuSubtype
+        }
+    }
+
+    var rawFileType: UInt32 {
+        switch self {
+        case let ._32(header):
+            return header.fileType
+        case let ._64(header):
+            return header.fileType
+        }
+    }
+
+    var rawFlags: UInt32 {
+        switch self {
+        case let ._32(header):
+            return header.flags
+        case let ._64(header):
+            return header.flags
+        }
+    }
+
+    var commandCount: UInt32 {
+        switch self {
+        case let ._32(header):
+            return header.commandCount
+        case let ._64(header):
+            return header.commandCount
+        }
+    }
+
+    var commandSize: UInt32 {
+        switch self {
+        case let ._32(header):
+            return header.commandSize
+        case let ._64(header):
+            return header.commandSize
+        }
+    }
+}
+
+public extension Mach.Header {
+    var cpuType: Mach.CPUType { Mach.CPUType(cpuType: rawCpuType) }
+
+    var fileType: Mach.FileType { Mach.FileType(fileType: rawFileType) }
+
+    var readableFlag: Set<MachHeaderFlag> { Set<MachHeaderFlag>(rawValue: Int(rawFlags)) }
 }
