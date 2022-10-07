983df9949ad26a95fcbc0fdbd1438039cf81d1df
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-10-07 20:57:57 +0800

    [MachoParser] : CPUType add RawRepresentable

--- Sources/MachOParser/MachOTypes/CPU/CPUType.swift
+++ Sources/MachOParser/MachOTypes/CPU/CPUType.swift
@@ -24,6 +24,8 @@ public enum CPUType {
 
     init(cpuType: cpu_type_t) {
         switch cpuType {
+        case CPU_TYPE_ANY:
+            self = .any
         case CPU_TYPE_VAX:
             self = .vax
         case CPU_TYPE_MC680x0:
@@ -56,6 +58,51 @@ public enum CPUType {
             self = .powerpc64
         default:
             self = .any
+        }
+    }
+}
+
+extension CPUType: RawRepresentable {
+    public typealias RawValue = cpu_type_t
+
+    public init?(rawValue: RawValue) {
+        self.init(cpuType: rawValue)
+    }
+
+    public var rawValue: RawValue {
+        switch self {
+        case .any:
+            return CPU_TYPE_ANY
+        case .vax:
+            return CPU_TYPE_VAX
+        case .mc680X0:
+            return CPU_TYPE_MC680x0
+        case .x86:
+            return CPU_TYPE_X86
+        case .i386:
+            return CPU_TYPE_I386
+        case .x86_64:
+            return CPU_TYPE_X86_64
+        case .mc98000:
+            return CPU_TYPE_MC98000
+        case .hppa:
+            return CPU_TYPE_HPPA
+        case .arm:
+            return CPU_TYPE_ARM
+        case .arm64:
+            return CPU_TYPE_ARM64
+        case .arm64_32:
+            return CPU_TYPE_ARM64_32
+        case .mc88000:
+            return CPU_TYPE_MC88000
+        case .sparc:
+            return CPU_TYPE_SPARC
+        case .i860:
+            return CPU_TYPE_I860
+        case .powerpc:
+            return CPU_TYPE_POWERPC
+        case .powerpc64:
+            return CPU_TYPE_POWERPC64
         }
     }
 }
