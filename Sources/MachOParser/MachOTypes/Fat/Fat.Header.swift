26e83c9a5b358a789e79e1e23291b3ef86eb382e
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-10-07 10:55:33 +0800

    [MachoParser] : add Fat.Header

---
+++ Sources/MachOParser/MachOTypes/Fat/Fat.Header.swift
@@ -0,0 +1,23 @@
+//
+// Created by Mengyu Li on 2022/10/5.
+//
+
+@_implementationOnly import MachCore
+import struct MachO.fat.fat_header
+
+public extension Fat {
+    struct Header {
+        public let magic: UInt32
+        public let architectureCount: UInt32
+
+        init(_ fat_header: fat_header, byteOrder: ByteOrder) {
+            magic = fat_header.magic
+            switch byteOrder {
+            case .little:
+                architectureCount = fat_header.nfat_arch.bigEndian
+            case .big:
+                architectureCount = fat_header.nfat_arch
+            }
+        }
+    }
+}
